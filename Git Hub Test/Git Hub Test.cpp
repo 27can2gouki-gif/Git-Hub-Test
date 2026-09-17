e74a177a8103ee953c16ba792341327a7218a717
ryota tsuji <ryofu1205@yahoo.co.jp>
2026-09-17 13:31:27 +0900

    testtesttesttsttesttestest

--- Git Hub Test/Git Hub Test.cpp
+++ Git Hub Test/Git Hub Test.cpp
@@ -1,20 +1,62 @@
-﻿// Git Hub Test.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+const int MAX_NAME = 16;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct SPECIAL {
+	char name[MAX_NAME + 1];
+	int Strength;
+	int Perception;
+	int Endurance;
+	int Charisma;
+	int Intelligence;
+	int Agility;
+	int Luck;
+};
+
+void Show(const SPECIAL& special) {
+	cout << "名前 ：" << special.name << endl
+		<< "　 Strength：" << special.Strength << endl
+		<< "　 Perception：" << special.Perception << endl
+		<< "　 Endurance：" << special.Endurance << endl
+		<< "　 Charisma：" << special.Charisma << endl
+		<< "　 Intelligence：" << special.Intelligence << endl
+		<< "　 Agility：" << special.Agility << endl
+		<< "　 Luck：" << special.Luck << endl;
 }
 
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+int main() {
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	int f = 0;
+	int g = 0;
 
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
+	cout << "貴方の力強さはどのくらい？" << endl;
+	cin >> a;
+	cout << "貴方の感知能力はどのくらい？" << endl;
+	cin >> b;
+	cout << "貴方の持久力はどのくらい？" << endl;
+	cin >> c;
+	cout << "貴方のカリスマ性はどのくらい？" << endl;
+	cin >> d;
+	cout << "貴方の知能はどのくらい？" << endl;
+	cin >> e;
+	cout << "貴方の敏捷性はどのくらい？" << endl;
+	cin >> f;
+	cout << "貴方の運の良さはどのくらい？" << endl;
+	cin >> g;
+
+
+	SPECIAL special[] = {
+		{"貴方のSPECIALは" , a,b,c,d,e,f,g,},
+	};
+	int size = sizeof special / sizeof * special;
+
+	for (int i = 0; i < size; ++i) {
+		Show(special[i]);
+	}
+	cout << "　　		です！" << endl;
+}
